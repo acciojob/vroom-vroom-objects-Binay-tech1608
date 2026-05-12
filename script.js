15e5b909e2c1fd7eb38b21511601b393a65a8cdc
Binay Mann <82080713+Binay-tech1608@users.noreply.github.com>
2026-05-12 14:32:21 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,25 @@
 // Complete the js code
-function Car(make, model) {}
+function Car(make, model) {
+	constructor(make,model){
+		this._make = make;
+		this._model = model;
+	}
+	getMakeModel(){
+		return `${this.make} ${this.model}`;
+	}
+}
 
-function SportsCar(make, model, topSpeed) {}
+function SportsCar(make, model, topSpeed) extends Car {
+    constructor(make, model, topSpeed){
+		super(make, model,topSpeed){
+			this._topspeed = topSpeed;
+		}
+	}
+	getTopSpeed(){
+		return `${this.topSpeed}`;
+	}
+	
+}
 
 // Do not change the code below
 window.Car = Car;
